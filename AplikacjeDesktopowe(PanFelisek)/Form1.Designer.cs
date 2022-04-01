09faa0d11c266fd36bc20de4da0ef380c6390528
KGebski0036 <57415454+KGebski0036@users.noreply.github.com>
2022-04-01 12:32:08 +0200

    update dzinennik szkolny

---
+++ AplikacjeDesktopowe(PanFelisek)/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace Szkolny_Dziennik
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownYearOfbirth = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxSchoolClass = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewUczniowie = new System.Windows.Forms.DataGridView();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nazwisko = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RokUrodzenia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Klasa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonAddStudent = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYearOfbirth)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUczniowie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imie:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwisko:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(6, 53);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(109, 23);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(152, 53);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(109, 23);
+            this.textBoxSurname.TabIndex = 3;
+            this.textBoxSurname.TextChanged += new System.EventHandler(this.textBoxSurname_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDownYearOfbirth);
+            this.groupBox1.Controls.Add(this.comboBoxSchoolClass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxSurname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 93);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtry";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 57);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Szukaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rok urodzenia (0 dla wszystkich):";
+            // 
+            // numericUpDownYearOfbirth
+            // 
+            this.numericUpDownYearOfbirth.Location = new System.Drawing.Point(455, 54);
+            this.numericUpDownYearOfbirth.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.numericUpDownYearOfbirth.Name = "numericUpDownYearOfbirth";
+            this.numericUpDownYearOfbirth.Size = new System.Drawing.Size(180, 23);
+            this.numericUpDownYearOfbirth.TabIndex = 6;
+            this.numericUpDownYearOfbirth.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBoxSchoolClass
+            // 
+            this.comboBoxSchoolClass.FormattingEnabled = true;
+            this.comboBoxSchoolClass.Location = new System.Drawing.Point(300, 53);
+            this.comboBoxSchoolClass.Name = "comboBoxSchoolClass";
+            this.comboBoxSchoolClass.Size = new System.Drawing.Size(137, 23);
+            this.comboBoxSchoolClass.TabIndex = 5;
+            this.comboBoxSchoolClass.SelectedIndexChanged += new System.EventHandler(this.comboBoxSchoolClass_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(300, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Klasa:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonAddStudent);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(667, 93);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(133, 357);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operacje";
+            // 
+            // dataGridViewUczniowie
+            // 
+            this.dataGridViewUczniowie.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridViewUczniowie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUczniowie.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.Nazwisko,
+            this.RokUrodzenia,
+            this.Klasa});
+            this.dataGridViewUczniowie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewUczniowie.Location = new System.Drawing.Point(0, 93);
+            this.dataGridViewUczniowie.Name = "dataGridViewUczniowie";
+            this.dataGridViewUczniowie.RowTemplate.Height = 25;
+            this.dataGridViewUczniowie.Size = new System.Drawing.Size(667, 357);
+            this.dataGridViewUczniowie.TabIndex = 6;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NameColumn.DataPropertyName = "Name";
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.Name = "NameColumn";
+            // 
+            // Nazwisko
+            // 
+            this.Nazwisko.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nazwisko.DataPropertyName = "SurName";
+            this.Nazwisko.HeaderText = "Nazwisko";
+            this.Nazwisko.Name = "Nazwisko";
+            // 
+            // RokUrodzenia
+            // 
+            this.RokUrodzenia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.RokUrodzenia.DataPropertyName = "YearOfBirth";
+            this.RokUrodzenia.HeaderText = "RokUrodzenia";
+            this.RokUrodzenia.Name = "RokUrodzenia";
+            // 
+            // Klasa
+            // 
+            this.Klasa.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Klasa.DataPropertyName = "SchoolClass";
+            this.Klasa.HeaderText = "Klasa";
+            this.Klasa.Name = "Klasa";
+            // 
+            // buttonAddStudent
+            // 
+            this.buttonAddStudent.Location = new System.Drawing.Point(6, 35);
+            this.buttonAddStudent.Name = "buttonAddStudent";
+            this.buttonAddStudent.Size = new System.Drawing.Size(115, 51);
+            this.buttonAddStudent.TabIndex = 0;
+            this.buttonAddStudent.Text = "Dodaj Studnta";
+            this.buttonAddStudent.UseVisualStyleBackColor = true;
+            this.buttonAddStudent.Click += new System.EventHandler(this.buttonAddStudent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewUczniowie);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownYearOfbirth)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUczniowie)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewUczniowie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nazwisko;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RokUrodzenia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Klasa;
+        private System.Windows.Forms.ComboBox comboBoxSchoolClass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownYearOfbirth;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonAddStudent;
+    }
+}
+
