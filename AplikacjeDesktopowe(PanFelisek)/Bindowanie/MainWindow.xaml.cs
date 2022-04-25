ce85e4c4710b2960fe67e526c5bd356d243d913b
KGebski0036 <57415454+KGebski0036@users.noreply.github.com>
2022-04-25 11:57:29 +0200

    Add files via upload

---
+++ AplikacjeDesktopowe(PanFelisek)/Bindowanie/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Bindowanie 
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBlock.Text = "Value: " + textBox.Text;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
